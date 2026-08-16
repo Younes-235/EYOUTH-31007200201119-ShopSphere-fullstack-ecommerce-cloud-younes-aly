5267b2f6c876d41a87b5749e1a9b42102f961da3
Younes <younesaly207@gmail.com>
2026-08-16 23:16:52 +0300

    Vercel deployment

--- backend/index.js
+++ backend/index.js
@@ -3,17 +3,15 @@ require('dotenv').config();
 const app = require("./app.js");
 const connectMongoDB = require('./config/mongoDb.js');
 
-const PORT = process.env.PORT || 5000;
+connectMongoDB().catch(err => {
+  console.error('💥 Failed to connect to MongoDB:', err);
+});
 
-const startServer = async () => {
-  try {
-    await connectMongoDB();
-    app.listen(PORT, () => {
-      console.log(`🚀 Express e-commerce server is running on port ${PORT}`);
-    });
-  } catch (err) {
-    console.error('💥 Failed to connect to MongoDB:', err);
-  }
-};
+module.exports = app;
 
-startServer();
+if (process.env.NODE_ENV !== 'production') {
+  const PORT = process.env.PORT || 5000;
+  app.listen(PORT, () => {
+    console.log(`🚀 Express e-commerce server is running on port ${PORT}`);
+  });
+}
